c2bcd68f855caeb812374a16f8cd89141f5470fe
coinci <dayan13@live.cn>
2023-09-03 22:52:14 +0800

    upgrade material from 14 to 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule} from '@angular/material/icon';
-import { MatMenuModule} from '@angular/material/menu'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,11 +35,6 @@ import { NgrxPracticeComponent } from './ngrx-practice/ngrx-practice.component';
     ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatListModule,
-    MatGridListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
